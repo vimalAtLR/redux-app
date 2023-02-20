248ca8357a3fe69ac212e24b7d5eec9e86759246
vimalAtLR <vimal@logicrays.com>
2023-02-20 18:21:04 +0530

    redux setup

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='container'>
+        <h2>Increment/Decrement Amount</h2>
+        <button className="btn btn-primary mx-2"> + </button>
+        Add to cart
+        <button className="btn btn-primary mx-2"> - </button>
+    </div>
+  )
+}
+
+export default Shop
