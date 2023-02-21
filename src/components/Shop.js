546151903b6d62e20de77106e3379b71e8f3b53a
vimalAtLR <vimal@logicrays.com>
2023-02-21 13:13:23 +0530

    use of react-redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state/index'
+import { bindActionCreators } from 'redux';
 
 function Shop() {
+  const dispatch = useDispatch();
+  const { withdrawMoney, depositeMoney } = bindActionCreators(actionCreators, dispatch);
   return (
     <div className='container'>
         <h2>Increment/Decrement Amount</h2>
-        <button className="btn btn-primary mx-2"> + </button>
+        {/* <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}> - </button>
         Add to cart
-        <button className="btn btn-primary mx-2"> - </button>
+        <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.depositeMoney(100))}}> + </button> */}
+
+        <button className="btn btn-primary mx-2" onClick={() => {withdrawMoney(100)}}> - </button>
+        Add to cart
+        <button className="btn btn-primary mx-2" onClick={() => {depositeMoney(100)}}> + </button>
     </div>
   )
 }
